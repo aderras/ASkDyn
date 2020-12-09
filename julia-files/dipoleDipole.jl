1a6aec5fbce5c426f369b3ee5a2e2ece5a260812
Amel d <amel.derras@gmail.com>
2020-12-09 14:13:02 -0500

    Added comments

--- julia-files/dipoleDipole.jl
+++ julia-files/dipoleDipole.jl
@@ -1,9 +1,21 @@
+#=
+This module contains functions used to compute the dipolar energy of the
+spin lattice. We compute the interaction with convolution, which is why the
+module requires FFTW.
+=#
 module dipoleDipole
 
     using FFTW, PaddedViews
-    export FHD, convfft, buildVD, vddMatrices
+    export FHD, convfft, buildVD, vdMatrices
 
-    function vddMatrices(Nx::Int,Ny::Int,Nz::Int,pbc::Bool=false)
+    # We precompute matrices used to calculate the dipole-dipole interaction
+    # with this function. It's essentially the distance part of the equation.
+    #
+    # in: Nx, Ny, Nz = dimensions of the system, pbc = periodic boundary
+    # conditions == true
+    #
+    # out: Array of 4 (Nx,Ny) matrices
+    function vdMatrices(Nx::Int,Ny::Int,Nz::Int,pbc::Bool=false)
 
         phixx = Array{Float32}(undef,2*Nx, 2*Ny)
         phiyy = Array{Float32}(undef,2*Nx, 2*Ny)
@@ -19,9 +31,14 @@ module dipoleDipole
 
     end
 
+    # Builds one of the 4 types of VD matrices based on input "stype"
+    #
+    # in: stype = the dimension of the matrix we're working on (Options: "xx",
+    # "yy","zz","xy"), Nx,Ny,Nz are dimensions of the problem, PBC = periodic
+    # boundary conditions.
+    #
+    # out: (Nx,Ny) matrix
     function buildVD( stype::String, Nx, Ny, Nz, pbc )
-
-
 
         if pbc == true
 
@@ -48,6 +65,7 @@ module dipoleDipole
         return VD
     end
 
+    # The following functions come from the equation for dipole-dipole interaction
     @inline phixx(a,b) = if a==b==0 0. else (2.0* b^2-a^2)/denominator(a,b,0.) end
     @inline phiyy(a,b) = if a==b==0 0. else (2.0* a^2-b^2)/denominator(a,b,0.) end
     @inline phizz(a,b) = if a==b==0 0. else (-1)/(sqrt((a^2 + b^2)^2 * (a^2 + b^2))) end
@@ -58,6 +76,12 @@ module dipoleDipole
     @inline Fzz(nx,ny,Tx,Ty) = -nx*ny*(nx^2+ny^2)/(4*pi*Tx*Ty*(nx^2)*(ny^2)*sqrt(nx^2 + ny^2))
     @inline Fxy(nx,ny,Tx,Ty) = -1.0/(4*pi*Tx*Ty*sqrt(nx^2 + ny^2 ))
 
+    # Provides an element of the VD matrix with periodic boundary conditions
+    #
+    # in: nx,ny = position of VD matrix, stype = type of matrix, NX,Ny, and Nz
+    # are dimensions of the system
+    #
+    # out: variable 'sum,' which is element nx,ny of VD matrix of stype
     function Phipbc(nx::Int,ny::Int,stype::String, Nx, Ny, Nz)
 
         paramPbcX = round(Int,2000/float(Nx))
@@ -98,7 +122,12 @@ module dipoleDipole
 
     end
 
-    # Returns the xth and yth matrix element of phi
+    # Returns the nx-th and ny-th matrix element of VD of type stype
+    #
+    # in: nx, ny are coordinates of interest, stype is the kind of VD matrix
+    # being built, Nz is the dimension of the system in the z direction.
+    #
+    # out: variable 'sum,' which is the nx,ny element of VD
     function Phi(nx::Int, ny::Int, stype::String, Nz)
 
         dim = Nz
@@ -122,6 +151,7 @@ module dipoleDipole
 
         return sum
     end
+
 
     function sumphiyy(a,b,N)
         tot = 0.
@@ -156,6 +186,14 @@ module dipoleDipole
     end
 
 
+    # Computes the dipolar field (without multiplying by dipolar constant)
+    # by convolving and summing approproate dimensions of the spin matrix,
+    # mat, and the VD matrices, called phi here.
+    #
+    # in: mat = spin field, phi = VD matrices, pbc = periodic boundary cond.
+    #
+    # out: (3,Nx,Ny) array representing dipolar interaction of spin field mat
+    # at every point.
     function FHD(mat,phi,pbc::Bool=false)
 
         p,m,n = size(mat)
@@ -171,7 +209,14 @@ module dipoleDipole
     end
 
     # Convolution of two arrays using FFT
-    function convfft(a,b, pbc::Bool=false)#,pfor,planifft)
+    #
+    # in: matrices a and b, where a has dimensions (Nx,Ny), pbc = periodic
+    # boundary conditions. Need to know pbc because when true, we do not need
+    # to pad the input spin array. When pbc = false, input spin array is padded
+    # with zeros so that its dimensions match that of the vd matrix
+    #
+    # out = (Nx,Ny) matrix
+    function convfft(a,b, pbc::Bool=false)
 
         ax,ay = size(a)
         bx,by = size(b)
@@ -191,7 +236,15 @@ module dipoleDipole
 
     end
 
-
+    # This function splits a (3,Nx,Ny) array into a vector of 3 (Nx,Ny) arrays.
+    # This was implemented because there was too much memory allocation when
+    # using [1,:,:] operators in the FHD function. (Even with the @views macro.)
+    # There is probably a better way to do this wih native julia functions, but
+    # this works for now.
+    #
+    # in: (3, Nx, Ny) array
+    #
+    # out: [Bx,By,Bz] where each B is an (Nx,Ny) array
     function slicematrix(A::Array{Float64,3})
 
         p,m,n = size(A)
