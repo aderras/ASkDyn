b820f9facc4f8100eb241e774af9c996ed467b52
Amel Derras-Chouk <amel.derras@gmail.com>
2022-04-20 13:07:16 -0400

    changed run to refect new user inputs functions

--- 01-scripts/Run.jl
+++ 01-scripts/Run.jl
@@ -1,12 +1,9 @@
 #!/usr/bin/env julia1
 push!(LOAD_PATH, pwd())
-include("Params.jl")
 using Distributed, SpinDynamics, BenchmarkTools, UserInputs
 using InitialCondition, EffectiveField, Energy, EffectiveSize, Dipolar
 
-userParams = Params.buildUserInputParam()
-rp = UserInputs.paramRanges()
-
+allParams = UserInputs.paramObjs
 
 if userParams.cp.parallel == 1
 
@@ -20,10 +17,8 @@ if userParams.cp.parallel == 1
         using SpinDynamics, Distributed
         include("Params.jl")
 
-        function parallelize(userParams, rp)
+        function parallelize(allParams, rp)
           # All params is a list of all combinations of the requested inputs
-          allParams = []
-          Params.getparamlist!(userParams, allParams, rp)
           pmap(launchcomputation, allParams)
         end
 
@@ -33,10 +28,5 @@ if userParams.cp.parallel == 1
   Test.parallelize(userParams,rp)
 
 else
-
-    allParams = []
-    Params.getparamlist!(userParams, allParams, rp)
-
     map(launchcomputation, allParams)
-
 end
