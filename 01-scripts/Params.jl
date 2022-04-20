43c18b21db04200382f807f9e8ded38918dad3c5
Amel Derras-Chouk <amel.derras@gmail.com>
2022-04-20 13:02:21 -0400

    improved user inputs

--- 01-scripts/Params.jl
+++ 01-scripts/Params.jl
@@ -39,7 +39,7 @@ module Params
         nx::Int         # Lattice size in x
         ny::Int         # Lattice size in y
         nz::Int         # Lattice size in z
-        pbc::Float64    # Periodic boundary conditions
+        bc::Float64     # Periodic boundary conditions
 
         v               # Matrices to be used to compute DDI. It
                         # is advantageous to compute these once and
@@ -91,7 +91,7 @@ module Params
         width::Float64      # Width of impact
         dx::Int           # x position (0 < dx < nx)
         dy::Int           # y position (0 < dy < ny)
-        jMat
+        # jMat
     end
 
     mutable struct currentParams
@@ -127,172 +127,6 @@ module Params
         defect      # defectParams
         current     # current
         save        # saveChoices
-
-    end
-
-    # getTestParams creates a params struct to use for testing, so that the user
-    # does not need to navigate through user input every time. This function is
-    # also used to create a params struct of the same structure that is used in
-    # the program. It is useful in some areas when copying between structs has
-    # to be done.
-    function buildparam()
-
-        myMatParams = materialParams(0.0, 0.0, 0.0, 0.0, 0.0, 0, 0, 0, 0, [])
-        myCompParams = compParams(0, 0, 0.0, 0, 0.0, 0.0, 0.0, 0, 0, 0, 0,0.0,0,0.0,0)
-        myICParams = icParams("", 0, 0.0, 0, 0)
-        myPinningParams = pinningParams(0.0)
-        myDefectParams = defectParams(0, 0.0, 0.0, 0, 0, [])
-        mySaveChoices = saveChoices(0,0,0,0,0,0,0,0,0,0,0,0)
-        myCurrentChoices = currentParams(0.0, 0.0, 100)
-
-        # Now put all of the user choices into one struct
-        allParams = params(myMatParams, myCompParams, myICParams,
-            myPinningParams,myDefectParams, myCurrentChoices,
-            mySaveChoices)
-
-        return allParams
-
-    end
-
-    # Given a field and value, this searches the nested struct for approapriate
-    # location of the value. Works recursively. And slowly.
-    function setfield_nestedstruct!(nestedStruct, fieldname, value)
-
-        names = fieldnames(typeof(nestedStruct))
-
-        # If there are no fields, you reached the end of the nested structure
-        if length(names) == 0
-            return
-        # If the field is in this list, then set the value to what you want
-        elseif fieldname in names
-            setfield!(nestedStruct, fieldname, value)
-            return
-        # If the field is not in this level, it means there is another level to
-        # recurse through.
-        else
-            for f in names
-                setfield_nestedstruct!(getfield(nestedStruct, f), fieldname,
-                    value)
-            end
-        end
-    end
-
-    # Deep copies struct of structs. Works recursively. Source and dest are
-    # structs with the same structure.
-    #
-    # There is probably a native Julia way to do this. Couldn't find it.
-    function copystruct!(source, dest)
-        fields = fieldnames(typeof(source))
-
-        # If there are no fields, you reached the end of the nested structure
-        if length(fields) == 0
-            return
-        # If the parent module of this field is not Core, there is another
-        # level of nested structs to dig through. Call copystruct! again.
-        elseif parentmodule(typeof(getfield(source, fields[1]))) != Core
-            # For all the fields, recursively call copyStruct
-            for f in fields
-                copystruct!(getfield(source, f), getfield(dest, f))
-            end
-
-        # If the parent module of this level is Core, you've reached the values
-        # to copy, so do it.
-        else
-            # For all the fields, copy values from source to destination
-            for f in fields
-                setfield!(dest, f, getfield(source, f))
-            end
-        end
-    end
-
-    # Set field of a nested struct given multiple assignments
-    function setfield_multi!(paramStruct, fields, values)
-        for fv in collect(zip(fields,values))
-            setfield_nestedstruct!(paramStruct, fv[1], fv[2])
-        end
-    end
-
-    # Builds a list of all the compuatation parameters requested by the user. It
-    # does this by reursively sesarching through the user requests to make
-    # combinations of all possible  Stores all user requests in the
-    # input array, paramList
-    function getparamlist!(allParams, paramList, rangeParams, iters=[],
-        iCurr=[], count=1)
-
-        # Get field names of the range struct
-        fields = fieldnames(typeof(rangeParams))
-        vals=[getfield(rangeParams,f) for f in fields]
-        if all(length.(vals).==0) push!(paramList,allParams); return end
-        iters=[1:length(vec) for vec in vals]
-
-
-        for i in iters[count]
-            if count==1 iCurr=[] else iCurr=iCurr[1:(count-1)] end
-            push!(iCurr, i)
-
-            if count==length(iters)
-                values=[vals[k][iCurr[k]] for k in 1:length(iCurr)]
-                newParamElem = buildparam()
-                copystruct!(allParams, newParamElem)
-                setfield_multi!(newParamElem, fields, values)
-                push!(paramList, newParamElem)
-            else
-                getparamlist!(allParams, paramList, rangeParams, iters, iCurr, count+1)
-            end
-        end
-    end
-
-    function buildUserInputParam()
-
-        # Only build the following matrices if the dipolar interaction is nonzero
-        if UserInputs.Material.ed!=0.0
-            v = vdmatrices(UserInputs.Material.nx, UserInputs.Material.ny,
-                UserInputs.Material.nz, 1.0==UserInputs.Material.pbc)
-        else
-            v = [[0.0 0.0; 0.0 0.0],[0.0 0.0; 0.0 0.0],[0.0 0.0; 0.0 0.0],
-                [0.0 0.0; 0.0 0.0]]
-        end
-
-        # Create all the structs to be used by the rest of the program.
-        mp = materialParams(UserInputs.Material.j,
-            UserInputs.Material.h, UserInputs.Material.a, UserInputs.Material.dz,
-            UserInputs.Material.ed, UserInputs.Material.nx, UserInputs.Material.ny,
-            UserInputs.Material.nz, UserInputs.Material.pbc, v)
-
-        cp = compParams(UserInputs.Computation.solver,
-            UserInputs.Computation.maxSteps, UserInputs.Computation.dt,
-            UserInputs.Computation.nSteps, UserInputs.Computation.tol,
-            UserInputs.Computation.damping, UserInputs.Computation.T,
-            UserInputs.Computation.parallel, UserInputs.Computation.numCores,
-            UserInputs.Computation.runRelaxation, UserInputs.Computation.sMax,
-            UserInputs.Computation.faConst,UserInputs.Computation.nRot,
-            UserInputs.Computation.faTol, UserInputs.Computation.rChirality)
-
-        ic = icParams(UserInputs.InitialCondition.type,
-            UserInputs.InitialCondition.r, UserInputs.InitialCondition.chirality,
-            UserInputs.InitialCondition.icx, UserInputs.InitialCondition.icy)
-
-        pp = pinningParams(UserInputs.Pinning.pinField)
-
-        dp = defectParams(UserInputs.Defect.defType,
-            UserInputs.Defect.defStrength, UserInputs.Defect.defWidth,
-            UserInputs.Defect.defX, UserInputs.Defect.defY, [])
-
-        cc = currentParams(UserInputs.Current.xCurr,
-            UserInputs.Current.yCurr, UserInputs.Current.tOff)
-
-        sp = saveChoices(UserInputs.SaveChoices.totalE,
-            UserInputs.SaveChoices.exchE, UserInputs.SaveChoices.zeemE,
-            UserInputs.SaveChoices.dmiE, UserInputs.SaveChoices.pmaE,
-            UserInputs.SaveChoices.ddiE, UserInputs.SaveChoices.magn,
-            UserInputs.SaveChoices.size, UserInputs.SaveChoices.charge,
-            UserInputs.SaveChoices.loc, UserInputs.SaveChoices.fieldDuring,
-            UserInputs.SaveChoices.chirality)
-
-
-        userParams = params(mp, cp, ic, pp, dp, cc, sp)
-
-        return userParams
 
     end
 
--- 01-scripts/UserInputs.jl
+++ 01-scripts/UserInputs.jl
@@ -1,100 +1,103 @@
 module UserInputs
 
-    module Material
-        j = 1.0
-        h = 0.0 # Negative field is into the plane. +z points out of page
-        a = 0.0
-        ed = 0.0
-        dz = 0.0 #4*pi*ed # Negative dz is into the plane
-        nx = 1024
-        ny = nx
-        nz = 1
-        pbc = 0.0 # OPTIONS: Float between 1.0 and 6.0,
-                  # 0.0=nothing, 1.0=pbc,
-                  # 2.0/3.0 = 1st order backward euler, linear/quadratic interp
-                  # 4.0/5.0 = 4th order backward euler, linear/quadratic interp
-                  # 6.0/7.0 = 4th central difference, linear/quadratic interp
+    import Params
+
+    function itemizedList(params...)
+      #=
+        Given an arbitrary list of items and lists of items, return a list of tuples
+        containing combinations of every argument and each item in the input list.
+      =#
+      return collect(Iterators.product(params...))
     end
 
-    module Computation
-        # Computation parameters
-        solver = 0      # 0=RK4, 1=RK5
-        maxSteps = 500    # This is the maximum number of computation steps
-        dt = 0.2
-        nSteps = 10
-        tol = 10^-5
-        damping = 0.0
-        T = 0.0
-        parallel = 0
-        numCores = 1
-        runRelaxation = 0  # 0=import data if it exists, otherwise build initial
-                           # condition and run LLG, 1=LLG with damping set to
-                           # 1.0, 2=FA relaxation
-        sMax = 1
-        faConst = 0.3
-        nRot = 10
-        faTol = 10^-4
-
-        rChirality = 20    # MAKE SURE THIS MAKES SENSE
+    function buildStructFromTuple(structName, inputTuple::Tuple)
+      #=
+        Given a struct and a tuple containing all elements of the struct, return
+        the filled-in struct
+      =#
+      return structName(inputTuple...)
     end
 
-    module InitialCondition
-        import UserInputs.Material
-        type = "skyrmion" # OPTIONS: "skyrmion", "domainWall"
-        r = 12.0
-        chirality = pi/2
-        icx = Material.nx/2
-        icy = Material.ny/2
+    function buildItemizedStruct(structName, itemizedArgs)
+      #=
+        Given a struct name and a list of tuples each containing the arguments of
+        the struct, return a list of structs of type structName.
+      =#
+      return buildStructFromTuple.(structName, itemizedArgs)
     end
 
-    module Pinning
-        pinField = 0.0
+    j = 1.0
+    h = [0.0,-0.01,-0.1] # Negative field is into the plane. +z points out of page
+    a = 0.0
+    ed = 0.0
+    dz = 0.0 #4*pi*ed # Negative dz is into the plane
+    nx = 64
+    ny = nx
+    nz = 1
+    bc = 0.0  # OPTIONS: Float between 1.0 and 6.0,
+              # 0.0=nothing, 1.0=pbc,
+              # 2.0/3.0 = 1st order backward euler, linear/quadratic interp
+              # 4.0/5.0 = 4th order backward euler, linear/quadratic interp
+              # 6.0/7.0 = 4th central difference, linear/quadratic interp
+
+    if ed!=0.0
+      v = [vdmatrices(UserInputs.Material.nx, UserInputs.Material.ny,
+          UserInputs.Material.nz, 1.0==UserInputs.Material.pbc)]
+    else
+      v = [[[0.0 0.0; 0.0 0.0],[0.0 0.0; 0.0 0.0],[0.0 0.0; 0.0 0.0],
+          [0.0 0.0; 0.0 0.0]]]
     end
 
-    module Defect
-        defType = 0.0 # 0.0=None, 1.0=point, 2.0=Gaussian
-        defStrength = 0.0
-        defWidth = 0.0
-        defX = 0
-        defY = 0
-    end
+    # Computation parameters
+    solver = 0      # 0=RK4, 1=RK5
+    maxSteps = 10    # This is the maximum number of computation steps
+    dt = 1/2^3
+    nSteps = 1
+    tol = 10^-5
+    damping = 0.0
+    T = 0.0
+    parallel = 0
+    numCores = 1
+    runRelaxation = 0  # 0=import data if it exists, otherwise build initial
+                       # condition and run LLG, 1=LLG with damping set to
+                       # 1.0, 2=FA relaxation
+    sMax = 1
+    faConst = 0.3
+    nRot = 10
+    faTol = 10^-4
 
-    module Current
-        xCurr = 0.0
-        yCurr = 0.0
-        tOff = 0
-    end
+    rChirality = 20    # MAKE SURE THIS MAKES SENSE
 
-    module SaveChoices
-        totalE = 0
-        exchE = 0
-        zeemE = 0
-        dmiE = 0
-        pmaE = 0
-        ddiE = 0
-        magn = 0
-        size = 0
-        charge = 0
-        loc = 0
-        fieldDuring = 0
-        chirality = 0
-    end
+    type = ["skyrmion"] # OPTIONS: "skyrmion", "domainWall"
+    r = 3.0
+    chirality = pi/2
+    icx = nx/2
+    icy = ny/2
 
-    #=
-        If user would like to run the script for a range of values, store
-        those values in the following struct. Elements of this struct are
-        1d arrays.
+    pinField = 0.0
 
-        The name of each field in paramRanges must exactly match the name of
-        the field in the original struct. All structs are located in Params.jl
-    =#
-    Base.@kwdef mutable struct paramRanges
-        r = [5.0:20.0;]
-        # damp = [0.01,1.0]
-        # pbc = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0]
-        # dz = [0.00001,0.0001,0.001,0.01,0.1]
-        # h = [-0.0001,-0.001,-0.01,-0.1,0.01,0.001,0.0001]
-    end
+    defType = 0.0 # 0.0=None, 1.0=point, 2.0=Gaussian
+    defStrength = 0.0
+    defWidth = 0.0
+    defX = 0
+    defY = 0
+
+    xCurr = 0.0
+    yCurr = 0.0
+    tOff = 0
+
+    totalE = 1
+    exchE = 0
+    zeemE = 0
+    dmiE = 0
+    pmaE = 0
+    ddiE = 0
+    magn = 0
+    size = 1
+    charge = 1
+    loc = 0
+    fieldDuring = 0
+    chirality = 1
 
     # All the paths for importing and exporting files
     module Paths
@@ -110,11 +113,11 @@ module UserInputs
 
         # Choose labels and parameters here. Make sure order and elements match
         # elements of values(p).
-        labels = ["bc","nx","ny","nz","j","h","a","dz","ed","r","dt"]
+        labels = ["damp","t","bc","nx","ny","nz","j","h","a","dz","ed","r","dt"]
         labels = [string("_",l) for l in labels]
 
         function values(p)
-            return [p.mp.pbc, string(p.mp.nx), p.mp.ny, p.mp.nz, p.mp.j, p.mp.h,
+            return [p.cp.damp, p.mp.T, p.mp.bc, string(p.mp.nx), p.mp.ny, p.mp.nz, p.mp.j, p.mp.h,
                     p.mp.a, p.mp.dz, p.mp.ed, p.ic.r, p.cp.dt*p.cp.nn]
         end
 
@@ -141,10 +144,33 @@ module UserInputs
         finalField = "s_final"
         chirality = "gamma"
 
-
         # The names in this list have to match saveChoices options
         allFilenames = [energy,exch,zee,dmi,pma,ddi,magn,size,charge,loc,finalField,chirality]
 
     end
+
+
+    ## END USER INPUTS ########################################################
+    mpItems = itemizedList(j, h, a, ed, dz, nx, ny, nz, bc, ed)
+    cpItems = itemizedList(solver, maxSteps, dt, nSteps, tol, damping,
+        T, parallel, numCores, runRelaxation, sMax, faConst, nRot, faTol,
+        rChirality)
+    icItems = itemizedList(type, r, chirality, icx, icy)
+    pinItems = itemizedList(pinField)
+    defectItems = itemizedList(defType, defStrength, defWidth, defX, defY)
+    currItems = itemizedList(xCurr, yCurr, tOff)
+    saveItems = itemizedList(totalE, exchE, zeemE, dmiE, pmaE, ddiE, magn, size, charge,
+                loc, fieldDuring, chirality)
+
+    mp = buildItemizedStruct(Params.materialParams, mpItems)
+    cp = buildItemizedStruct(Params.compParams, cpItems)
+    ic = buildItemizedStruct(Params.icParams, icItems)
+    pin = buildItemizedStruct(Params.pinningParams, pinItems)
+    dp = buildItemizedStruct(Params.defectParams, defectItems)
+    curr = buildItemizedStruct(Params.currentParams, currItems)
+    save = buildItemizedStruct(Params.saveChoices, saveItems)
+
+    paramItems = itemizedList(mp, [cp], [ic], [pin], [dp], [curr], [save])
+    paramObjs = buildItemizedStruct(Params.params, paramItems)
 
 end
